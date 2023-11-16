073db40b770328c1f51721f235a04e2ea5f446e8
Ashen <ashenrandika@gmail.com>
2023-11-16 14:40:51 +0100

    WIP on adding prisma

--- data/db.sql
+++ data/db.sql
@@ -1,9 +1,8 @@
 -- Enable pgcrypto extension for UUID generation
-CREATE
-EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
--- users table with user_type
-CREATE TABLE users
+-- user_profile table with user_type
+CREATE TABLE user_profile
 (
     user_id    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email      TEXT NOT NULL UNIQUE,
@@ -11,41 +10,41 @@ CREATE TABLE users
     password   TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name  TEXT NOT NULL,
-    user_type  TEXT NOT NULL    DEFAULT 'User',
+    user_type  TEXT NOT NULL DEFAULT 'User',
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- vm_templates table
-CREATE TABLE vm_templates
+-- vm_template table
+CREATE TABLE vm_template
 (
     template_id   UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     template_name TEXT NOT NULL UNIQUE,
-    cpu           INT  NOT NULL,
-    ram           INT  NOT NULL,
-    storage       INT  NOT NULL,
+    cpu           INT NOT NULL,
+    ram           INT NOT NULL,
+    storage       INT NOT NULL,
     os            TEXT NOT NULL,
     description   TEXT,
     created_at    TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at    TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- virtual_machines table with template_id reference
-CREATE TABLE virtual_machines
+-- virtual_machine table with template_id reference
+CREATE TABLE virtual_machine
 (
     vm_id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id     UUID REFERENCES users (user_id) ON DELETE CASCADE,
-    template_id UUID REFERENCES vm_templates (template_id) ON DELETE SET NULL,
+    user_id     UUID REFERENCES user_profile (user_id) ON DELETE CASCADE,
+    template_id UUID REFERENCES vm_template (template_id) ON DELETE SET NULL,
     vm_name     TEXT NOT NULL,
     created_at  TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- vm_events table
-CREATE TABLE vm_events
+-- vm_event table
+CREATE TABLE vm_event
 (
     event_id    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    vm_id       UUID REFERENCES virtual_machines (vm_id) ON DELETE CASCADE,
+    vm_id       UUID REFERENCES virtual_machine (vm_id) ON DELETE CASCADE,
     event_type  TEXT NOT NULL,
     description TEXT,
     timestamp   TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
@@ -55,8 +54,8 @@ CREATE TABLE vm_events
 CREATE TABLE user_activity
 (
     activity_id   UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id       UUID REFERENCES users (user_id) ON DELETE CASCADE,
-    vm_id         UUID REFERENCES virtual_machines (vm_id) ON DELETE SET NULL,
+    user_id       UUID REFERENCES user_profile (user_id) ON DELETE CASCADE,
+    vm_id         UUID REFERENCES virtual_machine (vm_id) ON DELETE SET NULL,
     activity_type TEXT NOT NULL,
     description   TEXT,
     timestamp     TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
