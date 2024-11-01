5b00983bc45caafc0b9e4431225451b4a57621e6
Ashen Wijesiri <ashenrandika@gmail.com>
2024-11-01 15:33:38 +0100

    Setting up resource limits for Redis pod. Changes to logger.

--- rest/src/utils/logger.ts
+++ rest/src/utils/logger.ts
@@ -1,18 +1,16 @@
 import { createLogger, format, transports } from 'winston'
-import { LoggingWinston } from '@google-cloud/logging-winston'
 
 const {colorize, combine, timestamp, printf, errors, json} = format
 
 const env = process.env.NODE_ENV || 'development'
 
-const myFormat = printf(({level, message, timestamp, stack}) => {
+const devFormat = printf(({level, message, timestamp, stack}) => {
   return `${timestamp} [${level}] ${stack || message}`
 })
 
-const loggingWinston = new LoggingWinston( {
-  prefix: env === 'development' ? 'local-dev' : undefined,
-})
-
+const transportsList = [
+  new transports.Console(),
+]
 
 // Create a Winston logger instance
 const logger = createLogger({
@@ -20,25 +18,17 @@ const logger = createLogger({
   format: combine(
     timestamp(),
     errors({stack: true}), // Include stack traces
-    env === 'development' ? combine(colorize(), myFormat) : json() // Use JSON format in production
+    env === 'development' ? combine(colorize(), devFormat) : json() // Use JSON format in production
   ),
   defaultMeta: {service: 'rest'},
-  transports: [
-    new transports.Console(),
-    loggingWinston,
-  ],
+  transports: transportsList,
 })
 
 // Handle uncaught exceptions
-logger.exceptions.handle(
-  new transports.Console(),
-  loggingWinston
-)
+logger.exceptions.handle(...transportsList)
+
 
 // Handle unhandled rejections
-logger.rejections.handle(
-  new transports.Console(),
-  loggingWinston
-)
+logger.rejections.handle(...transportsList)
 
 export default logger
