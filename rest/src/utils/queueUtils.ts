cb221148367cd3ffeef781cb39bfc49167c0f045
Ashen Randika <ashenr@users.noreply.github.com>
2026-01-12 13:22:05 +0100

    Dep update (#43)
    * Update dependencies
    Refactor RabbitMQ connection handling to use ChannelModel and improve error handling
    
    * Update dependencies in yarn.lock to latest versions

--- rest/src/utils/queueUtils.ts
+++ rest/src/utils/queueUtils.ts
@@ -1,29 +1,29 @@
-import amqp from 'amqplib'
+import * as amqp from 'amqplib'
 import * as Sentry from '@sentry/node'
 import * as MessageQueueService from '../services/messageQueueService'
 import logger from './logger'
+import { ChannelModel } from 'amqplib'
 
-let connection: amqp.Connection | null = null
-export let channel: amqp.Channel | null = null
+export let channel: amqp.ConfirmChannel | null = null
 export let isChannelOpen = false
 const rabbitMqUrl = process.env.RABBITMQ_URL_GCP || 'amqp://localhost:5672'
 
 export async function connectToRabbitMQ() {
   try {
     logger.info({message: 'Connecting to RabbitMQ'})
-    connection = await amqp.connect(rabbitMqUrl)
+    const channelModel: ChannelModel = await amqp.connect(rabbitMqUrl)
     logger.info({message: 'Connected to RabbitMQ'})
 
-    channel = await connection.createConfirmChannel()
+    channel = await channelModel.createConfirmChannel()
     isChannelOpen = true
 
-    connection.on('close', () => {
+    channelModel.on('close', () => {
       isChannelOpen = false
       logger.warn({message: 'Connection to RabbitMQ closed, reconnecting...'})
       reconnect()
     })
 
-    connection.on('error', (err) => {
+    channelModel.on('error', (err) => {
       logger.error({message: 'RabbitMQ connection error', error: err})
     })
 
@@ -37,4 +37,3 @@ export async function connectToRabbitMQ() {
 function reconnect() {
   setTimeout(connectToRabbitMQ, 5000) // Reconnect after 5 seconds
 }
-
